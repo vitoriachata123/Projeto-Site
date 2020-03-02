6159201419108c5438fb80f714490dceddc70389
Dinno2255 <61020830+Dinno2255@users.noreply.github.com>
2020-03-02 17:17:35 -0300

    Nova configuração
    Node configurado

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path =require("path");
+const app=express();
+//const mongoose = require("mongoose");
+//configs
+//body parser
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+//handlebars
+app.engine('handlebars',handlebars({defaultLayout:'main'}))
+app.set('view engine','handlebars');
+//public
+app.use(express.static(path.join(__dirname,"public")))
+//rotas
+app.get("/",function(req,res){
+    res.render('admin/index')
+})
+
+app.get("/sobre",function(req,res){
+    res.send("Sla")
+})
+
+//outros
+const PORT =8081
+app.listen(PORT,function(){
+    console.log("Servidor Rodando")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const express = require("express");
+const app = express();
+
+
+
+
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/html/index.html")
+})
+
+app.get("/sobre", function(req,res){
+    res.send("Minha pag Sobre")
+})
+app.get("/regras", function(req,res){
+    res.send("Bem vindo as regras")
+})
+app.get('/ola/:nome/:profissao/:cor',function(req,res){
+    res.send("Ola "+ req.params.nome+ "  Seu cargo e: "+ req.params.profissao)
+    
+})
+
+
+
+
+
+app.listen(8081,function(){
+    console.log("Rodando")
+});*/
